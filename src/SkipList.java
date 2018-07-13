1da4bfb1036f20c8f19dc8ff020b292cdef11eb1
CarlosML27 <CarlosML27@outlook.com>
2018-07-13 19:54:50 +0200

    Added classes to extend and implement with default needed methods

--- src/SkipList.java
+++ src/SkipList.java
@@ -1,1 +1,16 @@
+import java.io.Serializable;
+import java.util.AbstractSequentialList;
+import java.util.List;
+import java.util.ListIterator;
 
+public class SkipList<E extends Comparable<E>> extends AbstractSequentialList<E> implements List<E>, Cloneable, Serializable{
+    @Override
+    public int size(){
+        return 0;
+    }
+
+    @Override
+    public ListIterator<E> listIterator(int i) {
+        return null;
+    }
+}
